312d6d4b68a160b1702c807ded46cc80ed11ccf1
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-12 15:42:10 +0530

    Changes by Angelo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,33 @@
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { RecoilRoot } from "recoil";
+import type {AppProps} from "next/app";
+import {RecoilRoot} from "recoil";
 import "../../styles/globals.scss";
-import { ApolloProvider } from "@apollo/client";
+import {ApolloProvider} from "@apollo/client";
 import client from "../apollo-client/client";
+import {DefaultSeo} from 'next-seo';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Radiant Covid</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <RecoilRoot>
-        <ApolloProvider client={client}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      </RecoilRoot>
-    </>
-  );
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <DefaultSeo title={"Radiant covid"}
+                        description={"A website to help covid patients and let them find relevant information about condition."}
+                        openGraph={{
+                            title: "#COVID19India",
+                            locale: "en-IN",
+                            description: "We are here to help."
+                        }}
+                        twitter={{
+                            handle: "@rispatna",
+                            site: 'http://radiantpatna.com',
+                            cardType: 'summary_large_image',
+                        }}
+            />
+            <RecoilRoot>
+                <ApolloProvider client={client}>
+                    <Component {...pageProps} />
+                </ApolloProvider>
+            </RecoilRoot>
+        </>
+    );
 }
 
 export default MyApp;
