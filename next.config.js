8eea01af0762dbfb6f1e2f0901f14d3b04861b46
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-14 01:46:41 +0530

    removed unnecessary import

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { createSecureHeaders } = require("next-secure-headers");
 
 const withPwa = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-const withOffline = require('next-offline')
+
 
 
 const config = {
@@ -21,5 +21,6 @@ const config = {
   target: "serverless"
 }
 
-module.exports = withOffline(withPwa(config))
+module.exports = withPwa(config)
+
 
