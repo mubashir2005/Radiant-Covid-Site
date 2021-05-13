e03cd94d7cf3177370ba5a0123b426cf8eb59ecc
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-14 01:45:01 +0530

    Now will work offline

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { createSecureHeaders } = require("next-secure-headers");
 
 const withPwa = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const withOffline = require('next-offline')
 
 
 const config = {
@@ -15,11 +16,10 @@ const config = {
     dest: 'public',
     disable: false,
     register: true,
-    runtimeCaching,
-    sw: 'service-worker.js',
+    runtimeCaching
   },
   target: "serverless"
 }
 
-module.exports = withPwa(config)
+module.exports = withOffline(withPwa(config))
 
