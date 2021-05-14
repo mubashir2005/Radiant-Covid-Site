fff788b4e1dee81550c35ce032f0375361d1594a
Angelo <madhav1004@icloud.com>
2021-05-14 14:22:49 +0530

    changed the config a bit

--- next.config.js
+++ next.config.js
@@ -1,25 +1,20 @@
 const { createSecureHeaders } = require("next-secure-headers");
 
-
-const withPwa = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
-
-
+const withPwa = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 const config = {
   // other next withPwa
   async headers() {
     return [{ source: "/(.*)", headers: createSecureHeaders() }];
   },
-  future: {webpack5: true},
+  future: { webpack5: true },
   pwa: {
-    dest: 'public',
-    disable: false,
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
     register: true,
-    runtimeCaching
-  }
-}
+    runtimeCaching,
+  },
+};
 
-module.exports = withPwa(config)
-
-
+module.exports = withPwa(config);
