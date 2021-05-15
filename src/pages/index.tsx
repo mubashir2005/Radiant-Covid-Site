84328e7d72d6a7cf9f823a6fa90035ecc09fb6ea
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-15 21:21:15 +0530

    fixed all issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
 import Header from "../components/Header/Header";
 import App from "../components/App/App";
+import { COVID_19_INDIA } from "../constants";
+import { GetTweet } from "../server/typeDefs/typescript-types";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import queryState from "../atoms/query";
+import filtersState from "../atoms/filters";
+import tweetsState from "../atoms/tweets";
+import useSearchTweets from "../hooks/useSearchTweets";
+
+interface Props {
+  tweets: GetTweet[];
+}
+
+export default function Home({ tweets }: Props) {
+  const [query] = useRecoilState(queryState);
+  const [filters] = useRecoilState(filtersState);
+  const [_tweets, setTweets] = useRecoilState(tweetsState);
+  const { searchTweets } = useSearchTweets(query);
+
+  useEffect(() => {
+    if (query === COVID_19_INDIA && !filters.length && tweets.length) {
+      setTweets({
+        tweets: tweets,
+        loading: false,
+        error: null,
+      });
+      return;
+    }
+
+    searchTweets();
+  }, [query, filters, tweets]);
 
 
-
-export default function Home() {
   return (
     <>
       <Header />
