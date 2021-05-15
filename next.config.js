b673c8260dd58378722df80c9cd7da001880f02b
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-15 21:04:51 +0530

    fixed all issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const { createSecureHeaders } = require("next-secure-headers");
-const withPwa = require("next-offline");
+const withPwa = require("next-pwa");
 
 const config = {
-  async headers() {
-    return [{ source: "/(.*)", headers: createSecureHeaders() }];
-  },
   future: {
     webpack5: true,
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,9 @@
 import Header from "../components/Header/Header";
 import App from "../components/App/App";
-import resolvers from "../server/resolvers/resolvers";
-import { COVID_19_INDIA } from "../constants";
-import { useRouter } from "next/router";
-import { GetTweet } from "../server/typeDefs/typescript-types";
-import { useEffect } from "react";
-import { useRecoilState } from "recoil";
-import queryState from "../atoms/query";
-import filtersState from "../atoms/filters";
-import tweetsState from "../atoms/tweets";
-import useSearchTweets from "../hooks/useSearchTweets";
 
-interface Props {
-  tweets: GetTweet[];
-}
 
-export default function Home({ tweets }: Props) {
-  const { isFallback } = useRouter();
-  const [query] = useRecoilState(queryState);
-  const [filters] = useRecoilState(filtersState);
-  const [_tweets, setTweets] = useRecoilState(tweetsState);
 
-  const { searchTweets } = useSearchTweets(query);
-
-  useEffect(() => {
-    if (query === COVID_19_INDIA && !filters.length && tweets.length) {
-      setTweets({
-        tweets: tweets,
-        loading: false,
-        error: null,
-      });
-      return;
-    }
-
-    searchTweets();
-  }, [query, filters, tweets]);
-
-  if (isFallback) return <></>;
-
+export default function Home() {
   return (
     <>
       <Header />
@@ -48,32 +14,3 @@ export default function Home({ tweets }: Props) {
   );
 }
 
-export async function getStaticProps() {
-  const revalidationTime: number = 60; // 1 hour in seconds
-
-  try {
-    const tweets = await resolvers.Query.tweets(undefined, {
-      input: {
-        query: COVID_19_INDIA,
-      },
-    });
-
-    const tweetsId: GetTweet[] = tweets.map((tweet) => ({
-      id: tweet.id,
-    }));
-
-    return {
-      props: {
-        tweets: tweetsId,
-      },
-      revalidate: revalidationTime,
-    };
-  } catch (e) {}
-
-  return {
-    props: {
-      tweets: [],
-    },
-    revalidate: revalidationTime,
-  };
-}
