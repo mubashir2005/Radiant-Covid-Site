90ecf29672f5c17369426ee62720ba1c5c6b16d7
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-16 15:21:13 +0530

    added test using mocha

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    }
-};
