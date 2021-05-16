40dcad90035533613336fdf42f3f085f6522f777
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-05-16 12:44:09 +0530

    trying to fix bugs

--- src/apollo-client/client.ts
+++ src/apollo-client/client.ts
@@ -4,26 +4,25 @@ import {
   from,
   InMemoryCache,
 } from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
 
 const httpLink = createHttpLink({
   uri: `${typeof window !== "undefined" && window.location.origin}/api/graphql`,
 });
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
+// const errorLink = onError(({ graphQLErrors, networkError }) => {
+//   if (graphQLErrors)
+//     graphQLErrors.forEach(({ message, locations, path }) =>
+//       console.log(
+//         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+//       )
+//     );
+//
+//   if (networkError) console.log(`[Network error]: ${networkError}`);
+// });
 
 const client = new ApolloClient({
   ssrMode: typeof window === "undefined",
-  link: from([errorLink, httpLink]),
+  link: from([httpLink]),
   cache: new InMemoryCache(),
 });
 
