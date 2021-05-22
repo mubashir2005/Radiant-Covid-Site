8e297ee5e4fe66e30cfbcdbfb993e91aa18bf0e9
angelo1104 <madhav1004@icloud.com>
2021-05-22 11:52:05 +0000

    added icu link and my image.

--- src/pages/more.tsx
+++ src/pages/more.tsx
@@ -1,55 +1,80 @@
 import React from "react";
 
 function more() {
-
-  let styles={
-    cursor:"pointer",
-    textDecoration:"underline"
-  }
+  let styles = {
+    cursor: "pointer",
+    textDecoration: "underline",
+  };
   return (
     <div className={"more"}>
       <h1 className={"more__header"}> All Covid Resources in one place</h1>
       <div className={"more__links"}>
-
-        <a href={"https://shortnner.tech/QnsDuT_Y6"} target={'_blank'} className={"more__link"}>
+        <a
+          href={"https://shortnner.tech/QnsDuT_Y6"}
+          target={"_blank"}
+          className={"more__link"}
+        >
           <strong>Vaccine Registration</strong>
         </a>
         <br />
         <br />
-        <a href={"https://shortnner.tech/gmXKr2ABm"} target={'_blank'} className={"more__link"}>
+        <a
+          href={"https://shortnner.tech/gmXKr2ABm"}
+          target={"_blank"}
+          className={"more__link"}
+        >
           <strong>Book Vaccine Slots</strong>
         </a>
 
         <br />
         <br />
-        <a href="https://shortnner.tech/2fcQ23i01" className={"more__link"} target={'_blank'}>
+        <a
+          href="https://shortnner.tech/2fcQ23i01"
+          className={"more__link"}
+          target={"_blank"}
+        >
           <strong>MOHFW</strong>
         </a>
         <br />
         <br />
-        <a href="https://shortnner.tech/Tw4D8BI7a" className={"more__link"} target={'_blank'} >
+        <a
+          href="https://shortnner.tech/Tw4D8BI7a"
+          className={"more__link"}
+          target={"_blank"}
+        >
           <strong>AIIMS Patna</strong>
         </a>
         <br />
         <br />
-        <a href="https://qrgo.page.link/wtmHD" className={"more__link"} target={'_blank'}>
+        <a
+          href="https://qrgo.page.link/wtmHD"
+          className={"more__link"}
+          target={"_blank"}
+        >
           <strong>E-PASS Bihar</strong>
         </a>
         <br />
         <br />
-        <a href="https://pmch.in/" className={"more__link"} target={'_blank'}>
+        <a href="https://pmch.in/" className={"more__link"} target={"_blank"}>
           <strong>PMCH</strong>
         </a>
         <br />
         <br />
-        <a href="http://igims.org/" className={"more__link"} target={'_blank'}>
+        <a href="http://igims.org/" className={"more__link"} target={"_blank"}>
           <strong>IGIMS</strong>
         </a>
         <br/>
         <br/>
-        <br/>
-        <br/>
-        <a href={"/"} style={styles} ><em>Back to home</em></a>
+        <a href="https://covid19health.bihar.gov.in/DailyDashboard/BedsOccupied">
+          <strong>Bihar ICU beds tracking</strong>
+        </a>
+        <br />
+        <br />
+        <br />
+        <br />
+        <a href={"/"} style={styles}>
+          <em>Back to home</em>
+        </a>
       </div>
     </div>
   );
